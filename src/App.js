cf7b4a017863d71924bebe64f1e112ed085c0609
Suresh Kumar Mukhiya <itsmeskm99@gmail.com>
2019-10-16 22:36:41 +0200

    Add icon/promotions

--- src/App.js
+++ src/App.js
@@ -3,12 +3,27 @@ import React from 'react';
 import './App.css';
 import { quiz } from './data/chapter1';
 import Quiz from 'react-quiz-component/lib/Quiz';
+import { Card, Icon, Avatar } from 'antd';
+const { Meta } = Card;
+
 
 function App() {
   return (
     <div className="App">
       <header className="App-header">
         <Quiz quiz={quiz} showInstantFeedback={true}/>
+        <Card
+          style={{ width: 300 }}
+            cover={
+              <img
+                alt="example"
+                src="https://gw.alipayobjects.com/zos/rmsportal/JiqGstEfoWAOHiTxclqi.png"
+              />
+            }
+          >
+            Coming soon...!
+            Visual Aids for exploratory data analysis
+          </Card>
       </header>
     </div>
   );
