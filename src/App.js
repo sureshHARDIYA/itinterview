c7831b8f8896cecd634ede255eab8827797d5dfb
Suresh Kumar Mukhiya <itsmeskm99@gmail.com>
2019-10-16 22:03:27 +0200

    add quiz

--- src/App.js
+++ src/App.js
@@ -1,23 +1,14 @@
 import React from 'react';
-import logo from './logo.svg';
+
 import './App.css';
+import { quiz } from './data/chapter1';
+import Quiz from 'react-quiz-component/lib/Quiz';
 
 function App() {
   return (
     <div className="App">
       <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
+        <Quiz quiz={quiz} showInstantFeedback={true}/>
       </header>
     </div>
   );
---
+++ src/data/chapter1.js
@@ -0,0 +1,101 @@
+export const quiz =  {
+  "quizTitle": "Fundamental aspects of exploratory data analysis",
+  "quizSynopsis": "The main objective of this chapter is to revise the fundamentals of EDA, what it is, key concepts of profiling and quality assessment main dimensions of EDA, main challenges and opportunities in EDA.",
+  "questions": [
+    {
+      "question": "Which task is not _performed_ in data cleaning stage?",
+      "questionDescription": "<pre>import numpy as np <br/>np.array(['1', '2', '3'])</pre>",
+      "questionType": "text",
+      "answerSelectionType": "single",
+      "answers": [
+        "duplicates check",
+        "error check",
+        "transform data",
+        "missing value check"
+      ],
+      "correctAnswer": "3",
+      "messageForCorrectAnswer": "Correct answer. Good job.",
+      "messageForIncorrectAnswer": "Incorrect answer. Please try again.",
+      "explanation": "Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. Ut enim ad minim veniam, quis nostrud exercitation ullamco laboris nisi ut aliquip ex ea commodo consequat.",
+      "point": "20"
+    },
+    {
+      "question": "Which of the following is true?",
+      "questionType": "text",
+      "answerSelectionType": "single",
+      "answers": [
+        "Data = Model + Error",
+        "Model = Data + Error",
+        "Model = Data + Error",
+        "Data = Model - Error",
+        "None"
+      ],
+      "correctAnswer": "1",
+      "messageForCorrectAnswer": "Correct answer. Good job.",
+      "messageForIncorrectAnswer": "Incorrect answer. Please try again.",
+      "explanation": "Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. Ut enim ad minim veniam, quis nostrud exercitation ullamco laboris nisi ut aliquip ex ea commodo consequat.",
+      "point": "20"
+    },
+    {
+      "question": "ReactJS is an MVC based framework?",
+      "questionType": "text",
+      "answerSelectionType": "single",
+      "answers": [
+        "True",
+        "False"
+      ],
+      "correctAnswer": "2",
+      "messageForCorrectAnswer": "Correct answer. Good job.",
+      "messageForIncorrectAnswer": "Incorrect answer. Please try again.",
+      "explanation": "Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. Ut enim ad minim veniam, quis nostrud exercitation ullamco laboris nisi ut aliquip ex ea commodo consequat.",
+      "point": "10"
+    },
+    {
+      "question": "Which of the following concepts is/are key to ReactJS?",
+      "questionType": "text",
+      "answerSelectionType": "single",
+      "answers": [
+        "Component-oriented design",
+        "Event delegation model",
+        "Both of the above",
+      ],
+      "correctAnswer": "3",
+      "messageForCorrectAnswer": "Correct answer. Good job.",
+      "messageForIncorrectAnswer": "Incorrect answer. Please try again.",
+      "explanation": "Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. Ut enim ad minim veniam, quis nostrud exercitation ullamco laboris nisi ut aliquip ex ea commodo consequat.",
+      "point": "30"
+    },
+    {
+      "question": "Lorem ipsum dolor sit amet, consectetur adipiscing elit,",
+      "questionType": "photo",
+      "answerSelectionType": "single",
+      "answers": [
+        "https://dummyimage.com/600x400/000/fff&text=A",
+        "https://dummyimage.com/600x400/000/fff&text=B",
+        "https://dummyimage.com/600x400/000/fff&text=C",
+        "https://dummyimage.com/600x400/000/fff&text=D"
+      ],
+      "correctAnswer": "1",
+      "messageForCorrectAnswer": "Correct answer. Good job.",
+      "messageForIncorrectAnswer": "Incorrect answer. Please try again.",
+      "explanation": "Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. Ut enim ad minim veniam, quis nostrud exercitation ullamco laboris nisi ut aliquip ex ea commodo consequat.",
+      "point": "20"
+    },
+    {
+      "question": "What are the advantages of React JS?",
+      "questionType": "text",
+      "answerSelectionType": "multiple",
+      "answers": [
+        "React can be used on client and as well as server side too",
+        "Using React increases readability and makes maintainability easier. Component, Data patterns improves readability and thus makes it easier for manitaining larger apps",
+        "React components have lifecycle events that fall into State/Property Updates",
+        "React can be used with any other framework (Backbone.js, Angular.js) as it is only a view layer"
+      ],
+      "correctAnswer": [1, 2, 4],
+      "messageForCorrectAnswer": "Correct answer. Good job.",
+      "messageForIncorrectAnswer": "Incorrect answer. Please try again.",
+      "explanation": "Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. Ut enim ad minim veniam, quis nostrud exercitation ullamco laboris nisi ut aliquip ex ea commodo consequat.",
+      "point": "20"
+    },
+  ]
+}
