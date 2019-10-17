9410124341d792df248fcfcebb74c95e6440bebb
sureshHARDIYA <itsmeskm99@gmail.com>
2019-10-17 09:40:36 +0200

    another attempt to fix redirect

--- src/data/chapter1.js
+++ src/data/chapter1.js
@@ -1,101 +1,108 @@
-export const quiz =  {
-  "quizTitle": "Fundamental aspects of exploratory data analysis",
-  "quizSynopsis": "The main objective of this chapter is to revise the fundamentals of EDA, what it is, key concepts of profiling and quality assessment main dimensions of EDA, main challenges and opportunities in EDA.",
-  "questions": [
+export const quiz = {
+  quizTitle: "Fundamental aspects of exploratory data analysis",
+  quizSynopsis:
+    "The main objective of this chapter is to revise the fundamentals of EDA, what it is, key concepts of profiling and quality assessment main dimensions of EDA, main challenges and opportunities in EDA.",
+  questions: [
     {
-      "question": "Which task is not _performed_ in data cleaning stage?",
-      "questionDescription": "<pre>import numpy as np <br/>np.array(['1', '2', '3'])</pre>",
-      "questionType": "text",
-      "answerSelectionType": "single",
-      "answers": [
+      question: "Which task is not _performed_ in data cleaning stage?",
+      questionDescription:
+        "<pre>import numpy as np <br/>np.array(['1', '2', '3'])</pre>",
+      questionType: "text",
+      answerSelectionType: "single",
+      answers: [
         "duplicates check",
         "error check",
         "transform data",
         "missing value check"
       ],
-      "correctAnswer": "3",
-      "messageForCorrectAnswer": "Correct answer. Good job.",
-      "messageForIncorrectAnswer": "Incorrect answer. Please try again.",
-      "explanation": "Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. Ut enim ad minim veniam, quis nostrud exercitation ullamco laboris nisi ut aliquip ex ea commodo consequat.",
-      "point": "20"
+      correctAnswer: "3",
+      messageForCorrectAnswer: "Correct answer. Good job.",
+      messageForIncorrectAnswer: "Incorrect answer. Please try again.",
+      explanation:
+        "Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. Ut enim ad minim veniam, quis nostrud exercitation ullamco laboris nisi ut aliquip ex ea commodo consequat.",
+      point: "20"
     },
     {
-      "question": "Which of the following is true?",
-      "questionType": "text",
-      "answerSelectionType": "single",
-      "answers": [
+      question: "Which of the following is true?",
+      questionType: "text",
+      answerSelectionType: "single",
+      answers: [
         "Data = Model + Error",
         "Model = Data + Error",
         "Model = Data + Error",
         "Data = Model - Error",
         "None"
       ],
-      "correctAnswer": "1",
-      "messageForCorrectAnswer": "Correct answer. Good job.",
-      "messageForIncorrectAnswer": "Incorrect answer. Please try again.",
-      "explanation": "Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. Ut enim ad minim veniam, quis nostrud exercitation ullamco laboris nisi ut aliquip ex ea commodo consequat.",
-      "point": "20"
+      correctAnswer: "1",
+      messageForCorrectAnswer: "Correct answer. Good job.",
+      messageForIncorrectAnswer: "Incorrect answer. Please try again.",
+      explanation:
+        "Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. Ut enim ad minim veniam, quis nostrud exercitation ullamco laboris nisi ut aliquip ex ea commodo consequat.",
+      point: "20"
     },
     {
-      "question": "ReactJS is an MVC based framework?",
-      "questionType": "text",
-      "answerSelectionType": "single",
-      "answers": [
-        "True",
-        "False"
-      ],
-      "correctAnswer": "2",
-      "messageForCorrectAnswer": "Correct answer. Good job.",
-      "messageForIncorrectAnswer": "Incorrect answer. Please try again.",
-      "explanation": "Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. Ut enim ad minim veniam, quis nostrud exercitation ullamco laboris nisi ut aliquip ex ea commodo consequat.",
-      "point": "10"
+      question: "Check the dataset given below, and select the correct type:",
+      questionDescription:
+        "<p>Let's say your teacher gives you a spelling test with ten words. You studied really hard, did your best, and got nine out of ten questions right. Well done! When you get home, your mom or dad may ask you how many questions you got right. If they do, your parents are actually collecting discrete data. The number of questions you answered correctly can be 1, 2, 3, 4, 5, 6, 7, 8, 9, or 10. </p>",
+      questionType: "text",
+      answerSelectionType: "single",
+      answers: ["True", "False"],
+      correctAnswer: "2",
+      messageForCorrectAnswer: "Correct answer. Good job.",
+      messageForIncorrectAnswer: "Incorrect answer. Please try again.",
+      explanation:
+        "Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. Ut enim ad minim veniam, quis nostrud exercitation ullamco laboris nisi ut aliquip ex ea commodo consequat.",
+      point: "10"
     },
     {
-      "question": "Which of the following concepts is/are key to ReactJS?",
-      "questionType": "text",
-      "answerSelectionType": "single",
-      "answers": [
+      question: "Which of the following concepts is/are key to ReactJS?",
+      questionType: "text",
+      answerSelectionType: "single",
+      answers: [
         "Component-oriented design",
         "Event delegation model",
-        "Both of the above",
+        "Both of the above"
       ],
-      "correctAnswer": "3",
-      "messageForCorrectAnswer": "Correct answer. Good job.",
-      "messageForIncorrectAnswer": "Incorrect answer. Please try again.",
-      "explanation": "Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. Ut enim ad minim veniam, quis nostrud exercitation ullamco laboris nisi ut aliquip ex ea commodo consequat.",
-      "point": "30"
+      correctAnswer: "3",
+      messageForCorrectAnswer: "Correct answer. Good job.",
+      messageForIncorrectAnswer: "Incorrect answer. Please try again.",
+      explanation:
+        "Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. Ut enim ad minim veniam, quis nostrud exercitation ullamco laboris nisi ut aliquip ex ea commodo consequat.",
+      point: "30"
     },
     {
-      "question": "Lorem ipsum dolor sit amet, consectetur adipiscing elit,",
-      "questionType": "photo",
-      "answerSelectionType": "single",
-      "answers": [
+      question: "Lorem ipsum dolor sit amet, consectetur adipiscing elit,",
+      questionType: "photo",
+      answerSelectionType: "single",
+      answers: [
         "https://dummyimage.com/600x400/000/fff&text=A",
         "https://dummyimage.com/600x400/000/fff&text=B",
         "https://dummyimage.com/600x400/000/fff&text=C",
         "https://dummyimage.com/600x400/000/fff&text=D"
       ],
-      "correctAnswer": "1",
-      "messageForCorrectAnswer": "Correct answer. Good job.",
-      "messageForIncorrectAnswer": "Incorrect answer. Please try again.",
-      "explanation": "Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. Ut enim ad minim veniam, quis nostrud exercitation ullamco laboris nisi ut aliquip ex ea commodo consequat.",
-      "point": "20"
+      correctAnswer: "1",
+      messageForCorrectAnswer: "Correct answer. Good job.",
+      messageForIncorrectAnswer: "Incorrect answer. Please try again.",
+      explanation:
+        "Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. Ut enim ad minim veniam, quis nostrud exercitation ullamco laboris nisi ut aliquip ex ea commodo consequat.",
+      point: "20"
     },
     {
-      "question": "What are the advantages of React JS?",
-      "questionType": "text",
-      "answerSelectionType": "multiple",
-      "answers": [
+      question: "What are the advantages of React JS?",
+      questionType: "text",
+      answerSelectionType: "multiple",
+      answers: [
         "React can be used on client and as well as server side too",
         "Using React increases readability and makes maintainability easier. Component, Data patterns improves readability and thus makes it easier for manitaining larger apps",
         "React components have lifecycle events that fall into State/Property Updates",
         "React can be used with any other framework (Backbone.js, Angular.js) as it is only a view layer"
       ],
-      "correctAnswer": [1, 2, 4],
-      "messageForCorrectAnswer": "Correct answer. Good job.",
-      "messageForIncorrectAnswer": "Incorrect answer. Please try again.",
-      "explanation": "Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. Ut enim ad minim veniam, quis nostrud exercitation ullamco laboris nisi ut aliquip ex ea commodo consequat.",
-      "point": "20"
-    },
+      correctAnswer: [1, 2, 4],
+      messageForCorrectAnswer: "Correct answer. Good job.",
+      messageForIncorrectAnswer: "Incorrect answer. Please try again.",
+      explanation:
+        "Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. Ut enim ad minim veniam, quis nostrud exercitation ullamco laboris nisi ut aliquip ex ea commodo consequat.",
+      point: "20"
+    }
   ]
-}
+};
